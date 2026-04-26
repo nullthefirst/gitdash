434a2fbffc291a763b79504c71374032b1346488
Tey - nullthefirst <usheninte234@gmail.com>
2026-04-26 23:13:41 +0100

    Updated font setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,1..1000&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
