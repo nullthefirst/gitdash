705c7d5b101d96384cba774869091a4d79ec84f4
Tey - nullthefirst <usheninte234@gmail.com>
2026-04-26 23:08:06 +0100

    Registered font with tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-// tailwind.config.ts
-const config = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   theme: {
     extend: {
-      keyframes: {
-        "progress-loading": {
-          "0%": { transform: "translateX(-100%)" },
-          "50%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(100%)" },
+      fontFamily: {
+        sans: ["var(--font-google-sans)", "ui-sans-serif", "system-ui"],
+        keyframes: {
+          "progress-loading": {
+            "0%": { transform: "translateX(-100%)" },
+            "50%": { transform: "translateX(0%)" },
+            "100%": { transform: "translateX(100%)" },
+          },
         },
-      },
-      animation: {
-        "progress-loading": "progress-loading 2s infinite ease-in-out",
+        animation: {
+          "progress-loading": "progress-loading 2s infinite ease-in-out",
+        },
       },
     },
   },
